08de4e9ab7e84ce3b9d1e71ba8cb033f9628a2b9
Cesar Salgado <cesarsvs@gmail.com>
2014-06-09 03:23:55 -0300

    Added option to sort the file names before return.
    In the method get_all_file_names_from_dir.

--- io.py
+++ io.py
@@ -3,11 +3,13 @@ from os import listdir
 import re
 
 # if ext is specified then pattern is ignored.
-def get_all_file_names_from_dir(path_to_dir, ext=None, pattern=".*"):
+def get_all_file_names_from_dir(path_to_dir, ext=None, pattern=".*", sort=False):
   files = []
   if ext:
     pattern = ".*\.%s$" % ext
   for f in listdir(path_to_dir): 
     if isfile(join(path_to_dir, f)) and re.match(pattern, f):
       files.append(f)
+  if sort:
+    files.sort()
   return files
