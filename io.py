b913684ae848594d8658112c197a08cb848346c0
Cesar Salgado <cesarsvs@gmail.com>
2014-08-28 22:25:29 -0300

    Added option to return list of filenames with path

--- io.py
+++ io.py
@@ -3,13 +3,14 @@ from os import listdir
 import re
 
 # if ext is specified then pattern is ignored.
-def get_all_file_names_from_dir(path_to_dir, ext=None, pattern=".*", sort=False):
+def get_all_file_names_from_dir(path_to_dir, ext=None, pattern=".*", sort=False, withpath=False):
   files = []
   if ext:
     pattern = ".*\.%s$" % ext
   for f in listdir(path_to_dir): 
-    if isfile(join(path_to_dir, f)) and re.match(pattern, f):
-      files.append(f)
+    pathfile = join(path_to_dir, f)
+    if isfile(pathfile) and re.match(pattern, f):
+      files.append(pathfile if withpath else f)
   if sort:
     files.sort()
   return files
