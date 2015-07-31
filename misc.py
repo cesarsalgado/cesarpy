b4b26d8393178940be9236f34bc34b24dfc47249
Cesar Salgado <cesarsvs@gmail.com>
2015-07-31 18:18:37 -0300

    Added get_screen_reso method.

--- misc.py
+++ misc.py
@@ -1,4 +1,8 @@
 import datetime
+import subprocess
 
 def get_current_date_and_time_str():
     return datetime.datetime.today().strftime("%Y_%m_%d_%H_%M_%S")
+
+def get_screen_reso():
+    return tuple(map(int, subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True,stdout=subprocess.PIPE).communicate()[0].strip().split('x')))
